6861cc874604dd90c15dd3d86571e272b514e053
JakubLesinski30 <jakub.lesinski30@gmail.com>
2023-10-12 14:11:42 +0200

    first commit

---
+++ src/main/java/PortfolioJL/JakubLesinski/JakubLesinskiApplication.java
@@ -0,0 +1,13 @@
+package PortfolioJL.JakubLesinski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakubLesinskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakubLesinskiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PortfolioJL/JakubLesinski/JakubLesinskiApplicationTests.java
@@ -0,0 +1,13 @@
+package PortfolioJL.JakubLesinski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakubLesinskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
